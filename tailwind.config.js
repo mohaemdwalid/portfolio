2e2e21babb33845d623d7e17ae73dc9145849b28
mohaemdwalid <mohamedwalid680@gmail.com>
2025-05-17 10:35:44 +0300

    Fix TailwindCSS and PostCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ export default {
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
+        "black-50": "#1c1c21",
         "white-100": "#f3f3f3",
+        "white-50": "#d9ecff"
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -23,6 +25,11 @@ export default {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
       },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['group-hover'],
     },
   },
   plugins: [],
